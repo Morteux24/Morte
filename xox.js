105302172eefc724152033cf84066bd315f50c58
Morteux24 <morteux3169@gmail.com>
2025-03-28 21:54:12 +0400

    Create xox.js

---
+++ xox.js
@@ -0,0 +1,9 @@
+// xox.js
+document.getElementById("game-content").innerText = "XOX oyunu başlatılıyor...";
+
+function startXoxGame() {
+    // XOX oyununun başlangıcı ve mantığı burada olacak
+    console.log("XOX oyunu başladı!");
+}
+
+startXoxGame();  // XOX oyununu başlat
