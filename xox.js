a80126a68738dad396718803e30938954edd55f0
Morteux24 <morteux3169@gmail.com>
2025-03-28 22:36:42 +0400

    Delete xox.js

--- xox.js
+++
@@ -1,9 +0,0 @@
-// xox.js
-document.getElementById("game-content").innerText = "XOX oyunu başlatılıyor...";
-
-function startXoxGame() {
-    // XOX oyununun başlangıcı ve mantığı burada olacak
-    console.log("XOX oyunu başladı!");
-}
-
-startXoxGame();  // XOX oyununu başlat
