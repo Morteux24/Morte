31d4428ac47fee2591c276b216853daa1b8a0ccb
Morteux24 <morteux3169@gmail.com>
2025-03-28 21:54:38 +0400

    Create mines.js

---
+++ mines.js
@@ -0,0 +1,9 @@
+// mines.js
+document.getElementById("game-content").innerText = "Mayın Tarlası oyunu başlatılıyor...";
+
+function startMinesGame() {
+    // Mayın Tarlası oyununun başlangıcı ve mantığı burada olacak
+    console.log("Mayın Tarlası oyunu başladı!");
+}
+
+startMinesGame();  // Mayın Tarlası oyununu başlat
