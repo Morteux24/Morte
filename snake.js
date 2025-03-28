5fbfa504699055c0596dc95fefc9be7e03642bcc
Morteux24 <morteux3169@gmail.com>
2025-03-28 21:53:18 +0400

    Create snake.js

---
+++ snake.js
@@ -0,0 +1,9 @@
+// snake.js
+document.getElementById("game-content").innerText = "Yılan oyunu başlatılıyor...";
+
+function startSnakeGame() {
+    // Yılan oyununun başlangıcı ve mantığı burada olacak
+    console.log("Yılan oyunu başladı!");
+}
+
+startSnakeGame();  // Yılan oyununu başlat
